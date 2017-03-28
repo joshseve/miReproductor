6c4fd6883250985417a56885d8c1734208785bea
Paola Jocelin Septimo Velazquez <Paola Jocelin Septimo Velazquez>
2017-03-28 09:10:15 -0600

    Version mejorada

--- assets/js/reproductorHTML5.js
+++ assets/js/reproductorHTML5.js
@@ -1,4 +1,34 @@
+var miRola = document.getElementById("miRola");
 
-function reproducir(){
-document.getElementById("miRola").play();
+function playAudio(){
+miRola.play();
 }
+
+function pauseAudio(){
+miRola.pause();
+}
+
+function stopAudio(){
+miRola.load();
+}
+
+function subirVolumen(){
+miRola.volume+=0.1;
+}
+
+function bajarVolumen(){
+miRola.volume-=0.1;
+}
+
+function adelantar(){
+miRola.currentTime+=15;
+}
+
+function atrasar(){
+miRola.currentTime-=15;
+ }
+
+miRola.ontimeupdate = function() {
+var rango = document.getElementById("rango")
+rango.value = miRola.currentTime;
+};
